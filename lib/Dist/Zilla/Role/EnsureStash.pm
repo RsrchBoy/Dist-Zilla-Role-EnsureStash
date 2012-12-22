3e1f48d6989de7e7fa291524d94e7e2733ba1c97
Chris Weyl <cweyl@alumni.drew.edu>
2012-12-22 15:30:41 -0600

    initial commit

---
+++ lib/Dist/Zilla/Role/EnsureStash.pm
@@ -0,0 +1,19 @@
+package Dist::Zilla::Role::EnsureStash;
+
+# ABSTRACT: The great new Dist::Zilla::Role::EnsureStash!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
